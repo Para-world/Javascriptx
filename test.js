3333bfa8db42a532fbf866ed2984371535879e56
Deepak kumar <dkumar39223@gmail.com>
2025-12-03 10:01:30 +0530

    done

---
+++ test.js
@@ -0,0 +1,48 @@
+//create a function in javscript which shows the area of a circle;
+//write a function in javascript which shows the volume of cube;
+//write a function which find outs which was the leap year or not?
+//write a function which find outs the  factorial of n numbers;
+
+let a = 56;
+
+if (a >= 50){
+    console.log("greater than 50");
+} else {
+    console.log("less than 50");
+}
+
+
+let b = 45;
+let c = 60;
+
+if (b > c){
+    console.log("b is greater than c");
+} else if (b < c){
+    console.log("b is less than c");
+} else {
+    console.log("b is equal to c");
+}
+
+let age = 78;
+
+if (age >= 18){
+    console.log("you are able to vote")
+} else {
+    console.log("you are not able to vote")
+}
+
+
+
+//student grade card
+
+let marks = 86;
+
+// if (marks >= 90) and (marks <= 100){
+//     console.log("Grade A")
+// } else if (marks >= 80){
+//     console.log("Grade B")
+// } else if (marks >= 70){
+//     console.log("Grade C")
+// } else if (marks >= 60){
+//     console.log("Grade D")
+// }
